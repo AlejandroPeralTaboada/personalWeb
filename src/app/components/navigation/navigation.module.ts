94947b23332f64e5c04c237ec6f70c01c44dd9ed
Alejandro Peral Taboada <alejandropta@ext.inditex.com>
2018-06-22 09:19:43 +0200

    titulos

--- src/app/components/navigation/navigation.module.ts
+++ src/app/components/navigation/navigation.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NavbarItemComponent } from './navbar/navbar-item/navbar-item.component';
 import { SearchbarComponent } from './searchbar/searchbar.component';
+import { NavTitleComponent } from './navbar/nav-title/nav-title.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [NavbarComponent, NavbarItemComponent, SearchbarComponent],
+  declarations: [NavbarComponent, NavbarItemComponent, SearchbarComponent, NavTitleComponent],
   exports: [NavbarComponent, SearchbarComponent]
 })
 export class NavigationModule { }
