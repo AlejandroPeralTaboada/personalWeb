12deea33fad2ede62f253b16564f9d2b28e90bbf
Alejandro Peral Taboada <alejandro.peral@udc.es>
2018-06-30 19:41:30 +0200

    configures menu

--- src/app/components/navigation/navbar/navbar-item/navbar-item.component.ts
+++ src/app/components/navigation/navbar/navbar-item/navbar-item.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 })
 export class NavbarItemComponent implements OnInit {
   @Input() menuName: String;
+  @Input() iconName: String;
   @Output() menuClicked: EventEmitter<String> = new EventEmitter();
 
   constructor() {}
